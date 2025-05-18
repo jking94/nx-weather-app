4942c4611d161a6abf3b4181c670c9b3926870da
jking94 <kingjeart@gmail.com>
2025-05-18 12:02:23 -0400

    added sourcemap and launch.json to enable debugging for nest backend application, added nest configuration service to grab env var from nx root

--- apps/weather-app-backend/src/app/app.module.ts
+++ apps/weather-app-backend/src/app/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/weather-app-backend/src/app/app.service.ts
+++ apps/weather-app-backend/src/app/app.service.ts
@@ -1,17 +1,20 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
 
-  constructor(private httpService: HttpService) {}
+  constructor(private httpService: HttpService, private configService: ConfigService) {}
 
   getData(): { message: string } {
     return { message: 'Hello API' };
   }
   async getUsers() {
-     const response = await firstValueFrom(this.httpService.get('https://jsonplaceholder.typicode.com/users'));
-     return response.data;
+    const ApiKey = this.configService.get<string>('OPEN_WEATHER_API_KEY');
+    console.log(ApiKey);
+    const response = await firstValueFrom(this.httpService.get('https://jsonplaceholder.typicode.com/users'));
+    return response.data;
   }
 }
