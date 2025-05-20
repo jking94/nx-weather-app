084260e3f002c2e9f1f84a386c2197c36b83398c
jking94 <kingjeart@gmail.com>
2025-05-20 19:43:19 -0400

    logging and metrics comments

--- apps/weather-app-backend/src/app/app.service.ts
+++ apps/weather-app-backend/src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 import { LocationDataModel } from './types/location-data-model';
@@ -8,6 +8,7 @@ import { mapWeatherErrorToStandardException } from './helpers';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name)
 
   constructor(private httpService: HttpService, private configService: ConfigService) {}
 
@@ -22,24 +23,35 @@ export class AppService {
 
   async getGeoCoderInfoByCity(cityName: string): Promise<GeoCoderDataModel> {
     try {
+      const startTime = new Date().getTime();
       const ApiKey = this.configService.get<string>('OPEN_WEATHER_API_KEY');
       const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${ApiKey}`
       const response = await firstValueFrom(this.httpService.get(url));
       const geoCoderDataModel: GeoCoderDataModel = response.data;
+      const endTime = new Date().getTime();
+      const callDuration = endTime - startTime;
+      //Log callDuration of API call to external service such as DataDog for metrics purposes
       return geoCoderDataModel;
     } catch (error) {
+      this.logger.error('Failed to GET GeoCoder data by cityName')
+      //Log to external service such as DataDog
         throw mapWeatherErrorToStandardException(error)
     }
   }
 
-    async getLocationInfoByLatLon(lat: number, lon: number) {
+  async getLocationInfoByLatLon(lat: number, lon: number) {
     try {
+      const startTime = new Date().getTime();
       const ApiKey = this.configService.get<string>('OPEN_WEATHER_API_KEY');
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${ApiKey}`
       const response = await firstValueFrom(this.httpService.get(url));
       const locationDataModel: LocationDataModel = response.data;
+      const endTime = new Date().getTime();
+      const callDuration = endTime - startTime;
+      //Log callDuration of API call to external service such as DataDog for metrics purposes
       return locationDataModel;
     } catch (error) {
+        this.logger.error('Failed to GET GeoCoder data by cityName')
         throw mapWeatherErrorToStandardException(error)
     }
   }
